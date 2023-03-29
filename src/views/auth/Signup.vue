44c7c6fde3bd5b09ab790dfc5a14bb1097749a3b
elafhelow <91556744+elafhelow@users.noreply.github.com>
2023-03-29 23:17:59 +0300

    elaf

--- src/views/auth/Signup.vue
+++ src/views/auth/Signup.vue
@@ -1,46 +1,113 @@
+
 <template>
-    <div class="app-content">
-        <b-field label="Email" v-bind:type="{'is-danger': $v.user.email.$invalid}">
-            <b-input type="email"
-                v-on:input="$v.user.email.$touch"
-                v-model="user.email"></b-input>
-        </b-field>
+    <html>
+  <head>
+    <link
+      href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css"
+      rel="stylesheet"
+    />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;900&display=swap"
+      rel="stylesheet"
+    />
+    <meta
+      name="viewport"
+      content="width=device-width,initial-scale=1,maximum-scale=1"
+    />
+ 
+  </head>
 
-        <div class="columns">
-            <div class="column is-half">
-                <b-field label="First Name" v-bind:type="{'is-danger': $v.user.firstName.$invalid}">
-                    <b-input type="text" 
-                        v-on:input="$v.user.firstName.$touch"
-                        v-model="user.firstName"></b-input>
-                </b-field>
+  <body class="min-h-screen bg-gray-100 text-gray-900 flex justify-center">
+    <div
+      class="max-w-screen-xl m-0 sm:m-20 bg-white shadow sm:rounded-lg flex justify-center flex-1"
+    >
+      <div class="lg:w-1/2 xl:w-5/12 p-6 sm:p-11">
+        
+        <div class="mt-11 flex flex-col items-center">
+          <h1 class="text-2xl xl:text-3xl font-extrabold">
+            Sign up for Todo
+          </h1>
+          <div class="w-full flex-1 mt-8">
+           
+           
+
+            <div class="mx-auto max-w-xs">
+                
+              <input
+                class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
+                type="text"
+                placeholder="name"
+              />
+              <input
+                class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
+                type="email"
+                placeholder="email"
+              />
+              <input
+              class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
+              type="password"
+              placeholder="Password"
+            />
+              <button
+                class="mt-5 tracking-wide font-semibold bg-indigo-500 text-gray-100 w-full py-4 rounded-lg hover:bg-indigo-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none"
+              >
+                <svg
+                  class="w-6 h-6 -ml-2"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path d="M16 21v-2a4 4 0 00-4-4H5a4 4 0 00-4 4v2" />
+                  <circle cx="8.5" cy="7" r="4" />
+                  <path d="M20 8v6M23 11h-6" />
+                </svg>
+                <span class="ml-3" type="is-primary" >
+                  Sign Up
+                </span>
+              </button>
+              <p class="mt-6 text-xs text-gray-600 text-center">
+                I agree to abide by templatana's
+                <a href="#" class="border-b border-gray-500 border-dotted">
+                  Terms of Service
+                </a>
+                and its
+                <a href="#" class="border-b border-gray-500 border-dotted">
+                  Privacy Policy
+                </a>
+              </p>
             </div>
-            <div class="column is-half">
-                <b-field label="Last Name" v-bind:type="{'is-danger': $v.user.lastName.$invalid}">
-                    <b-input type="text"
-                        v-on:input="$v.user.lastName.$touch"
-                        v-model="user.lastName"></b-input>
-                </b-field>
-            </div>
+          </div>
         </div>
-
-        <b-field label="Password" v-bind:type="{'is-danger': $v.user.password.$invalid}">
-            <b-input type="password" 
-                v-on:input="$v.user.password.$touch"
-                v-model="user.password"></b-input>
-        </b-field>
-
-        <div>
-            <b-button type="is-primary" v-on:click="signup"
-                v-bind:disabled="$v.user.$invalid"
-                v-bind:loading="isLoading">Create new account</b-button>
-        </div>
-
-        <div class="app-margin">
-            <p>I already have an account
-                <router-link :to="{name: 'Login'}">Log in</router-link>
-            </p>
-        </div>
+      </div>
+      <div class="flex-1 bg-indigo-100 text-center hidden lg:flex">
+        <div
+          class="m-12 xl:m-16 w-full bg-contain bg-center bg-no-repeat"
+          style="background-image: url('https://storage.googleapis.com/devitary-image-host.appspot.com/15848031292911696601-undraw_designer_life_w96d.svg');"
+        ></div>
+      </div>
     </div>
+    <div class="REMOVE-THIS-ELEMENT-IF-YOU-ARE-USING-THIS-PAGE hidden treact-popup fixed inset-0 flex items-center justify-center" style="background-color: rgba(0,0,0,0.3);">
+        <div class="max-w-lg p-8 sm:pb-4 bg-white rounded shadow-lg text-center sm:text-left">
+          
+          <h3 class="text-xl sm:text-2xl font-semibold mb-6 flex flex-col sm:flex-row items-center">
+            <div class="bg-green-200 p-2 rounded-full flex items-center mb-4 sm:mb-0 sm:mr-2">
+              <svg class="text-green-800 inline-block w-5 h-5" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="currentColor" viewBox="0 0 24 24"><path d="M11 5.882V19.24a1.76 1.76 0 01-3.417.592l-2.147-6.15M18 13a3 3 0 100-6M5.436 13.683A4.001 4.001 0 017 6h1.832c4.1 0 7.625-1.234 9.168-3v14c-1.543-1.766-5.067-3-9.168-3H7a3.988 3.988 0 01-1.564-.317z"></path></svg>
+              </div>
+            Free TailwindCSS Component Kit!
+          </h3>  
+          <p>I recently released Treact, a <span class="font-bold">free</span> TailwindCSS Component Kit built with React.</p>
+          <p class="mt-2">It has 52 different UI components, 7 landing pages, and 8 inner pages prebuilt. And they are customizable!</p>
+          <div class="mt-8 pt-8 sm:pt-4 border-t -mx-8 px-8 flex flex-col sm:flex-row justify-end leading-relaxed">
+            <button class="close-treact-popup px-8 py-3 sm:py-2 rounded border border-gray-400 hover:bg-gray-200 transition duration-300">Close</button>
+            <a class="font-bold mt-4 sm:mt-0 sm:ml-4 px-8 py-3 sm:py-2 rounded bg-purple-700 text-gray-100 hover:bg-purple-900 transition duration-300 text-center" href="https://treact.owaiskhan.me" target="_blank">See Treact</a>
+          </div>
+        </div>
+      </div>
+  </body>
+</html>
+
 </template>
 
 <script>
@@ -52,8 +119,7 @@ export default {
             isLoading: false,
             user: {
                 email: '',
-                firstName: '',
-                lastName: '',
+                name: '',
                 password: ''
             }
         }
@@ -61,8 +127,7 @@ export default {
     validations: {
         user: {
             email: {required, email},
-            firstName: {required},
-            lastName: {required},
+            name: {required},
             password: {required}
         }
     },
@@ -71,11 +136,10 @@ export default {
             this.isLoading = true
             let user = new Parse.User()
 
-            user.set('username', this.user.email)
+            user.set('name', this.user.name)
+            user.set('email', this.user.email)
             user.set('password', this.user.password)
-            user.set('first_name', this.user.firstName)
-            user.set('last_name', this.user.lastName)
-
+        
             user.signUp().then(result => {
                 console.log('success')
                 this.$buefy.toast.open({
